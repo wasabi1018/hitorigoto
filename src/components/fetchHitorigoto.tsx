7e29701d73040f3fac5ab32ae3ac45a26756a857
wasabi1018 <mkcs1018mtz@gmail.com>
2025-01-19 20:06:09 +0900

    React Helmet 削除　metaタグ使用でOGPが表示されるかテスト

--- src/components/fetchHitorigoto.tsx
+++ src/components/fetchHitorigoto.tsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import SectionButton from './section-button';
 import { Hitorigoto } from '../types/hitorigoto';
-import { Helmet } from 'react-helmet';
 
 const FetchHitorigoto = () => {
   const [hitorigoto, setHitorigoto] = useState<Hitorigoto | null>(null);
@@ -35,10 +34,7 @@ const FetchHitorigoto = () => {
       <div>
         {hitorigoto && <p>{hitorigoto.message}</p>}
         <img width={100} height={100}  src="https://i.pinimg.com/736x/c7/ba/5d/c7ba5dd7c0bcc0edc5d1aa56ce19a118.jpg" />
-        <Helmet>
-          <title>ヒトリゴト</title>
-          <meta property="og:image" content="https://i.pinimg.com/736x/c7/ba/5d/c7ba5dd7c0bcc0edc5d1aa56ce19a118.jpg" />
-        </Helmet>
+        <meta property="og:image" content="https://i.pinimg.com/736x/c7/ba/5d/c7ba5dd7c0bcc0edc5d1aa56ce19a118.jpg" />
         <SectionButton onClickEvent={handleButtonClick} buttonName='誰かのヒトリゴトを聞く' />
       </div>
     );
