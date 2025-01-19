8c35fd77d8ce0934e91496c9c44974de60356163
wasabi1018 <mkcs1018mtz@gmail.com>
2025-01-19 09:44:01 +0900

    React Helmet テスト用コード

--- src/components/fetchHitorigoto.tsx
+++ src/components/fetchHitorigoto.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import SectionButton from './section-button';
 import { Hitorigoto } from '../types/hitorigoto';
+import { Helmet } from 'react-helmet';
 
 const FetchHitorigoto = () => {
   const [hitorigoto, setHitorigoto] = useState<Hitorigoto | null>(null);
@@ -33,6 +34,11 @@ const FetchHitorigoto = () => {
     return (
       <div>
         {hitorigoto && <p>{hitorigoto.message}</p>}
+        <img width={100} height={100}  src="https://i.pinimg.com/736x/c7/ba/5d/c7ba5dd7c0bcc0edc5d1aa56ce19a118.jpg" />
+        <Helmet>
+          <title>ヒトリゴト</title>
+          <meta property="og:image" content="https://i.pinimg.com/736x/c7/ba/5d/c7ba5dd7c0bcc0edc5d1aa56ce19a118.jpg" />
+        </Helmet>
         <SectionButton onClickEvent={handleButtonClick} buttonName='誰かのヒトリゴトを聞く' />
       </div>
     );
