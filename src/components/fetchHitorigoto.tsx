e75530a666ab73d24ff45d4b23c152face14284c
wasabi1018 <mkcs1018mtz@gmail.com>
2025-01-26 13:06:07 +0900

    OGPテスト Head使用

--- src/components/fetchHitorigoto.tsx
+++ src/components/fetchHitorigoto.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import SectionButton from './section-button';
 import { Hitorigoto } from '../types/hitorigoto';
-import Head from 'next/head';
+import GenerateMetaData from './generateMetaData';
 
 const FetchHitorigoto = () => {
   const [hitorigoto, setHitorigoto] = useState<Hitorigoto | null>(null);
@@ -36,13 +36,7 @@ const FetchHitorigoto = () => {
         {hitorigoto && <p>{hitorigoto.message}</p>}
         <img width={100} height={100}  src="https://www.webtech.co.jp/blog/wp-content/uploads/2015/01/fb_1200x630.png" />
         <SectionButton onClickEvent={handleButtonClick} buttonName='誰かのヒトリゴトを聞く' />
-
-        <Head>
-        <title>誰かのヒトリゴト</title>
-        <meta property="og:title" content={hitorigoto ? hitorigoto.message : '誰かのヒトリゴト'} />
-        <meta property="og:description" content="あなたの心に響く、誰かのヒトリゴト" />
-        <meta property="og:image" content="https://www.webtech.co.jp/blog/wp-content/uploads/2015/01/fb_1200x630.png" />
-        </Head>
+        <GenerateMetaData />
       </div>
     );
 };
---
+++ src/components/generateMetaData.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const GenerateMetaData = () => {
+  const title = 'ヒトリゴト OGP';
+  const discription = 'あなたの心に響く、誰かのヒトリゴト';
+  const image = "https://www.webtech.co.jp/blog/wp-content/uploads/2015/01/fb_1200x630.png";
+
+  return (
+    <>
+      <Head>
+        <title>ヒトリゴト OGP</title>
+        <meta property='og:title' content={ title } />
+        <meta property='og:discription' content={ discription } />
+        <meta property='og:image' content={ image } />
+      </Head>
+    </>
+  )
+};
+  
+  export default GenerateMetaData;
